479d2f5947caacee8643e7a029f10ff87189be68
Lívia Macedo <macedolivia803@gmail.com>
2023-06-13 15:33:41 -0300

    atualização final

--- js/login1.js
+++ js/login1.js
@@ -1,20 +1,42 @@
-var usuarios = [
-    {"email": "Livia", "cpf": "123", "senha": "123"},
-    {"email": "", "cpf": "123", "senha": "123"},
-    {"email": "", "cpf": "123", "senha": "123"},
+<script>
+
+
+
+
+var usuariosDB = [
+    { usuario: "macedolivia803@gmail.com", senha: "123" },
+    { usuario: "27948535039", senha: "4321" },
+    { usuario: "30632214066", senha: "5678" },
+    { usuario: "29715601049", senha: "8675" },
 ];
 
-function Entrar() {
-    var usuario = document.getElementById('email').value.toLowerCase();
-    var senha = document.getElementById('senha').value;
+var usuarioValidar;
+var senhaValidar;
 
-    for (var u in usuarios) {
-        var us = usuarios[u];
-        if (us.login === usuario && us.senha === senha) {
-            window.location = "index.html";
-            return true;
-        }
+function Entrar() {
+   
+
+    usuarioValidar = document.getElementById("email").value;
+    senhaValidar = document.getElementById("senha").value;
+
+    let usuarioAchado = usuariosDB.find(user => user.usuario === usuarioValidar)
+    let senhaAchada = usuariosDB.find(password => password.senha === senhaValidar)
+
+    console.log(usuarioAchado)
+
+    if (usuarioAchado == undefined && senhaAchada == undefined) {
+        
+        window.alert("Usuário ou senha incorretos.")
+    } else if (usuarioAchado == undefined) {
+        
+        window.alert("Usuário ou senha incorretos.")
+    } else if (senhaAchada == undefined) {
+       
+        window.alert("Usuário ou senha incorretos")
+    } else {
+        
+        window.alert("Login correto!")
+        window.location.assign("./index.html")
     }
-    alert("Dados incorretos, tente novamente.");
-    return false;
 }
+</script>
